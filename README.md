364ec1441a203aad3b0a962eb011d77231075120
fonwash <fonwash@gmail.com>
2025-05-09 13:36:12 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # setup-env
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg?style=for-the-badge)](https://www.gnu.org/licenses/gpl-3.0)
+![Linux](https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black)
+![Bash](https://img.shields.io/badge/Bash-121011?style=for-the-badge&logo=gnubash&logoColor=white)
+
+This project is a bash script that automates the installation of ZSH, Oh My ZSH, creation of SSH keys, installation of dotfiles and dev tools. It's designed to make setting up a new machine as painless as possible.
+
+## Features
+
+- **ZSH Installation**
+- **Oh My ZSH Installation**
+- **Powerlevel10k Installation**
+- **SSH Key Creation**
+- **Dotfiles Setup**
+- **Dev Tools Installation**
+
+## Usage
+
+Simply clone this repository and run the setup script:
+
+```bash
+git clone git@github.com:FonWasH/setup-env.git && cd setup-env && ./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+G=$'\e[92m'
+R=$'\e[91m'
+Y=$'\e[33m'
+X=$'\e[0m'
+
+ZSHInstall() {
+	zsh_install=false
+	if ! command -v zsh &>/dev/null; then
+		if sudo -l &>/dev/null; then
+			sudo apt -y install zsh
+			clear
+			echo "ZSH installed."
+			zsh_install=true
+		else
+			clear
+			echo "You do not have sudo privileges. Installation cannot proceed."
+			return 1
+		fi
+	else
+		clear
+		echo "ZSH is already installed."
+		zsh_install=true
+	fi
+	if $zsh_install; then
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+		clear
+		echo "ZSH, Oh My ZSH and Powerlevel10k installed."
+	else
+		clear
+		echo "ZSH is not installed. Installation cannot proceed."
+	fi
+}
+
+SSHKey() {
+	mail=""
+	while [ -z "$mail" ]; do
+		read -p "Enter your email address: " mail
+	done
+	ssh-keygen -t ed25519 -C "$mail"
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_ed25519
+	clear
+	echo "Your public key is:"
+	echo ""
+	cat ~/.ssh/id_ed25519.pub
+}
+
+DotfilesInstall() {
+	repo=""
+	while [ -z "$repo" ]; do
+		read -p "Enter the URL of the dotfiles repository: " repo
+	done
+	git clone --bare $repo ~/dotfiles
+	setopt -s glob_dots
+	mv ~/dotfiles/* ~/
+	rm -rf ~/dotfiles
+	clear
+	echo "Dotfiles installed."
+}
+
+DevToolsInstall
+{
+	if sudo -l &>/dev/null; then
+		sudo apt -y install build-essential gdb valgrind vim manpages-dev cmake
+		clear
+		echo "Tools installed."
+	else
+		clear
+		echo "You do not have sudo privileges. Installation cannot proceed."
+		return 1
+	fi
+}
+
+Setup() {
+	clear
+	echo "Welcome $USER."
+	while true; do
+		echo ""
+		echo "What would you like to install?"
+		echo ""
+		echo "	${Y}1. ${G}ZSH, Oh My ZSH and Powerlevel10k${X}"
+		echo "	${Y}2. ${G}SSH Key${X}"
+		echo "	${Y}3. ${G}Dotfiles${X}"
+		echo "	${Y}4. ${G}Dev Tools${X}"
+		echo "	${Y}5. ${R}Exit${X}"
+		echo ""
+		read -p "Enter the number of the option you would like to install (${Y}1${X}/${Y}2${X}/${Y}3${X}/${Y}4${X}/${Y}5${X}): " option
+		case $option in
+		1)
+			ZSHInstall
+			;;
+		2)
+			SSHKey
+			;;
+		3)
+			DotfilesInstall
+			;;
+		4)
+			DevToolsInstall
+			;;
+		5)
+			clear
+			echo "Exiting..."
+			exit 0
+			;;
+		*)
+			clear
+			echo "Invalid option. Please try again."
+			;;
+		esac
+	done
+}
+
+Setup
